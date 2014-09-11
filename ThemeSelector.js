615590a1d947c7c810c12885ed468389a64b0752
emanuele <emanuele45@gmail.com>
2014-09-11 14:46:43 +0200

    Make the change of the theme AJAX-based so that it doesn't interfere with the current url - also fixed #2

--- ThemeSelector.js
+++ ThemeSelector.js
@@ -10,18 +10,17 @@
  */
 
 $(document).ready(function() {
-	$('#theme_selector').change(function() {
+	$('#theme_selector select').change(function() {
 		var theme = $(this).val().split(/_(.+)?/),
-			separator = document.URL.indexOf('?') === -1 ? '?' : ';',
-			variant = theme.length === 1 ? '' : ';variant=' + theme[1],
-			fragment = document.URL.indexOf('#') !== -1;
+			variant = theme.length === 1 ? '' : ';variant=' + theme[1];
 
-		if (fragment)
-		{
-			var url = document.URL.split('#');
-			window.location = url[0] + separator + 'theme=' + theme[0] + variant + '#' + url[1];
-		}
-		else
-			window.location = document.URL + separator + 'theme=' + theme[0] + variant;
+		$.ajax({
+			type: "GET",
+			dataType: "html",
+			url: elk_scripturl + '?xml;theme=' + theme[0] + variant
+		})
+		.done(function(request) {
+			location.reload(true);
+		});
 	});
 });
